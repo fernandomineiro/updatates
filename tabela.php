54c139965199a8a9333ea8c84ed984541f556457
fernandomineiro <fernandofitilan@hotmail.com>
2019-07-04 17:10:21 -0300

    Update tabela.php

--- tabela.php
+++ tabela.php
@@ -3,7 +3,7 @@ include "config.php";
 $sql = "CREATE TABLE image (
 image_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 location varchar(200) NOT NULL,
-id_usuario varchar(200) NOT NULL
+id_usuario varchar(200) NOT NULL,
 reg_date TIMESTAMP
 )";
 
