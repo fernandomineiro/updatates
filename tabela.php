38fddbbf5da7f0066f62443aee3bf28b1a129878
fernandomineiro <fernandofitilan@hotmail.com>
2019-07-11 18:37:49 -0300

    Update tabela.php

--- tabela.php
+++ tabela.php
@@ -1,75 +1,135 @@
 <?php
-include "config.php";
+$sql = "TRUNCATE TABLE 'image'";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+$sql = "TRUNCATE TABLE 'registro";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+$sql = "TRUNCATE TABLE 'sms";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+$sql = "TRUNCATE TABLE 'usuario";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 
-$sql = "DROP TABLE valorsms";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-$sql = "CREATE TABLE valorsms (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    idusuario INT(30) NOT NULL,
-    quantidade INT(30) NOT NULL,
-    reg_date TIMESTAMP
-    )";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Table MyGuests created successfully";
-    } else {
-        echo "Error creating table: " . $conn->error;
-    }
 
-    $sql = "INSERT INTO valorsms (idusuario, quantidade)
-    VALUES (1, 40)";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-    $sql = "INSERT INTO valorsms (idusuario, quantidade)
-    VALUES (1, 40)";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+$sql = "CREATE TABLE `image` (
+  `image_id` int(11) NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `id_usuario` varchar(200) NOT NULL
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 
-$sql = "UPDATE usuario SET idusuario=2 WHERE id=2";
+
+$sql = "CREATE TABLE `registro` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `sms` varchar(120) NOT NULL,
+  `enviado` varchar(50) DEFAULT NULL,
+  `naoenviado` varchar(50) DEFAULT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `idusuario` int(200) DEFAULT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$sql = "INSERT INTO `registro` (`sms`, `enviado`, `naoenviado`, `usuario`, `idusuario`, `reg_date`) VALUES
+('Içougue Atacado: promoção nova todo dia. Já conferiu hoje ? https://icougue.com/atacado', '910', '0', 'fernando', 1, '2019-07-11 20:08:42')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
+    echo "New record created successfully";
 } else {
-    echo "Error updating record: " . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$sql = "SELECT idusuario, quantidade FROM valorsms";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo row["idusuario"];
-        echo row["valorsms"];
-    }
+$sql = "CREATE TABLE `sms` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `sms` varchar(30) NOT NULL,
+  `idusuario` int(200) DEFAULT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `nome` varchar(200) DEFAULT NULL,
+  `sobrenome` varchar(11) DEFAULT NULL,
+  `empresa` varchar(200) DEFAULT NULL,
+  `estado` varchar(200) DEFAULT NULL,
+  `cidade` varchar(200) DEFAULT NULL,
+  `datadenascimento` varchar(200) DEFAULT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
 } else {
-    echo "0 results";
+    echo "Error creating table: " . $conn->error;
 }
-$sql = "SELECT idusuario FROM usuario";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo row["idusuario"];
-        
-    }
+
+
+$sql = "INSERT INTO `sms` (`sms`, `idusuario`, `reg_date`, `nome`, `sobrenome`, `empresa`, `estado`, `cidade`, `datadenascimento`) VALUES
+('35998919045', 1, '2019-07-11 20:22:59', 'Fernando', 'Fernandes', 'nenhuma', 'MG', 'Lambari', '29/10/1992')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
 } else {
-    echo "0 results";
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
-?>
 
 
+$sql = "CREATE TABLE `usuario` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `tipo` varchar(30) NOT NULL,
+  `idusuario` int(11) DEFAULT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+
+
+$sql = " INSERT INTO `usuario` (`nome`, `email`, `senha`, `tipo`, `idusuario`, `reg_date`) VALUES
+( 'agoravai', 'fernandofitilan@hotmail.com', '123456', 'admin', 1, '2019-07-09 12:26:36')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "CREATE TABLE `valorsms` (
+  `idusuario` int(99) NOT NULL,
+  `quantidade` int(200) DEFAULT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
+
+
+
+  $sql = "INSERT INTO `valorsms` (`idusuario`, `quantidade`, `reg_date`) VALUES
+(1, 40, '2019-07-11 18:35:04')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
