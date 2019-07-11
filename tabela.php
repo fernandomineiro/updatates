8fad913dbaa610283f10d5b61a5d844b114be0df
fernandomineiro <fernandofitilan@hotmail.com>
2019-07-11 15:23:35 -0300

    Update tabela.php

--- tabela.php
+++ tabela.php
@@ -1,5 +1,13 @@
 <?php
 include "config.php";
+DROP TABLE table_name
+$sql = "DROP TABLE valorsms";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 $sql = "CREATE TABLE valorsms (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     idusuario INT(30) NOT NULL,
@@ -30,6 +38,30 @@ $sql = "CREATE TABLE valorsms (
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
+$sql = "SELECT idusuario, quantidade FROM valorsms";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo row["idusuario"];
+        echo row["valorsms"];
+    }
+} else {
+    echo "0 results";
+}
+$sql = "SELECT idusuario FROM usuario";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo row["idusuario"];
+        
+    }
+} else {
+    echo "0 results";
+}
 ?>
 
 
